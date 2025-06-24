3d7462ee12b9b110406933a706ded8b53d0f50f4
Mc-Colour <lukemccullough1701@icloud.com>
2025-06-24 02:18:41 +0100

    changed login link, so that when logged in it changes to a logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,40 @@
-import React from "react"
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import React, {useContext} from "react"
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate} from "react-router-dom"
 import Horses from "./pages/Horses";
 import Auth from "./pages/Auth";
+import { AuthContext } from "./context/AuthContext";
 
 function Home() {
     return <h1>Welcome to Equiwelf</h1>;
 }
 
 function App() {
+    const { token, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    // handle logout when booton is pressed
+    const handleLogout = () => {
+        logout();
+        navigate("/auth"); // redirect to auth page after logout
+    };
     return (
-        <Router>
+        <>
             <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc"}}>
-                <Link to="/">Home</Link> | <Link to="/horses">My Horses</Link> | <Link to="/auth">Login/Register</Link>
+                <Link to="/">Home</Link> | <Link to="/horses">My Horses</Link> |
+                {!token ? (
+                    <Link to ="/auth">Login</Link>
+                ): (
+                    <button onClick={handleLogout} style={{ marginLeft: "1rem" }}>
+                        Logout
+                    </button>
+                )}
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/horses" element={<Horses />} />
                 <Route path="/auth" element={<Auth />} />
             </Routes>
-        </Router>
+        </>
     );
 }
 export default App;
